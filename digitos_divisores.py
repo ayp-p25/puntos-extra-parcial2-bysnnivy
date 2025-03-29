f2e39c2aa90151bd0b1a4e8e3279f2a6bfe37844
bysnnivy <efrain.gonzalez@iteso.mx>
2025-03-28 20:04:00 -0600

    Primera Prueba

--- digitos_divisores.py
+++ digitos_divisores.py
@@ -1,1 +1,12 @@
-# Codifica aquí tu programa
+numero = int(input())
+num = str(numero)
+divisores = 0
+i = 0
+
+while i < len(num):
+    digito = int(num[i])
+    if digito != 0 and numero % digito == 0:
+        divisores += 1
+    i += 1
+
+print(divisores)
